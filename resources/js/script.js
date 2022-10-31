86debed3b5623394b5002531d8a9f1b5aa4c0982
mvrius69 <xmariusro@gmail.com>
2022-10-31 18:39:51 +0200

    added animations

--- resources/js/script.js
+++ resources/js/script.js
@@ -13,3 +13,41 @@ close.addEventListener('click', () => {
     close.classList.add('hidden');
     open.classList.remove('hidden');
 })
+
+const tl = gsap.timeline({ defaults: { duration: 0.75, ease: 'Power3.easeOut' } });
+tl.fromTo('.hero__circle', { opacity: 0, scale: 0 }, { opacity: 1, scale: 1 });
+tl.fromTo('.hero__left-side', { opacity: 0, x: -100 }, { opacity: 1, x: 0 });
+tl.fromTo('.hero__icons', { opacity: 0, x: 100 }, { opacity: 1, x: 0 });
+tl.fromTo('.hero__bg-text', { opacity: 0, y: 100 }, { opacity: 1, y: 0 });
+
+const tlS = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.hero',
+        start: '10%',
+        end: '80%',
+        scrub: false,
+    }
+});
+
+tlS.fromTo('.services', { opacity: 0 }, { opacity: 1 })
+
+const tlS2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.hero',
+        start: '10%',
+        end: '60%',
+        scrub: true,
+    }
+});
+
+tlS2.fromTo('.services__circle', { opacity: 0, x: -150 }, { opacity: 1, x: 0, stagger: .1 });
+
+tlP = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.hero',
+        start: '20%',
+        end: '80%',
+        scrub: false,
+    }
+});
+tlP.fromTo('.projects__content-card', { opacity: 0, y: -100, scale: .8 }, { opacity: 1, y: 0, scale: 1, stagger: .1 });
